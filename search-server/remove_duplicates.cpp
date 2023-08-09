d0667770d47abd0fc3faaee90a6a156e607320fe
RafaelMagasumov <rafel.magasumov@mail.ru>
2023-08-09 16:20:55 +0300

    6 sprint

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -2,19 +2,19 @@
 #include<iostream>
 
 void RemoveDuplicates(SearchServer& search_server) {
-	std::vector<std::vector<std::string>>  helper;
+	std::set<std::set<std::string>>  helper;
 	std::set<int> duplicates;
 
 	for (auto it_f = search_server.begin(); it_f != search_server.end(); ++it_f) {
-		std::vector<std::string> help_vector;
+		std::set<std::string> help_vector;
 		for (const auto& word : search_server.GetWordFrequencies(*it_f)) {
 
-			help_vector.push_back(word.first);
+			help_vector.insert(word.first);
 		}
 		if (std::count(helper.begin(), helper.end(), help_vector)) {
 			duplicates.insert(*it_f);
 		}
-		else helper.push_back(help_vector);
+		else helper.insert(help_vector);
 
 
 	}
